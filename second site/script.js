f0a4374e2a0bc863b063d502f948825c7b6fdd8e
ronniecs <ronn1xk1ng@gmail.com>
2021-11-14 00:23:40 +0300

    rnn

---
+++ second site/script.js
@@ -0,0 +1,18 @@
+function yes() {
+    text = document.getElementById('text')
+    btnY = document.getElementById('yes')
+    btnN = document.getElementById('no')
+
+    text.innerHTML = "жи есть"
+    btnY.style.display = "none"
+    btnN.style.display = "none"
+}
+
+function no() {
+    btnN = document.getElementById('no')
+  
+    let w = (document.documentElement.clientWidth / 2) - 40
+    let h = (document.documentElement.clientHeight / 2) - 30
+    btnN.style.left = Math.random() * (w * 2) - w + 'px'
+    btnN.style.top = Math.random() * (h * 2) - h + 'px'
+  }
